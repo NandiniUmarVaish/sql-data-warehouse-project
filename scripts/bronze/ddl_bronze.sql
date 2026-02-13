a3acff46b9ff359b4f62d4c617615a08086ff1aa
Nandini <nandiniumarvaish303@gmail.com>
2026-02-13 12:02:02 +0530

    Update ddl_bronze.sql

--- scripts/bronze/ddl_bronze.sql
+++ scripts/bronze/ddl_bronze.sql
@@ -9,64 +9,72 @@ Script Purpose:
 ===============================================================================
 */
 
+DROP TABLE IF EXISTS db_bronze.crm_cust_info;
+CREATE TABLE db_bronze.crm_cust_info(
+	cst_id						 INT,
+	cst_key						 NVARCHAR(50),
+    cst_firstname				 NVARCHAR(50),
+    cst_lastname 				 NVARCHAR(50),
+    cst_marital_status			 NVARCHAR(50),
+    cst_gndr					 NVARCHAR(50),
+    cst_create_date 			 DATE 
+);
 
-DROP TABLE IF EXISTS dw_bronze.crm_cust_info;
-CREATE TABLE dw_bronze.crm_cust_info (
-    cst_id 					INT,
-    cst_key 				VARCHAR(50),
-    cst_firstname 			VARCHAR(50),
-    cst_lastname 			VARCHAR(50),
-    cst_marital_status 		VARCHAR(50),
-    cst_gndr 				VARCHAR(50),
-    cst_create_date 		DATE
+DROP TABLE IF EXISTS db_bronze.crm_prd_info;
+CREATE TABLE db_bronze.crm_prd_info(
+	prd_id 				INT,
+    prd_key 			NVARCHAR(50),
+    prd_nm 				NVARCHAR(50),
+    prd_cost  			INT,
+    prd_line  			NVARCHAR(20),
+    prd_start_dt 		DATETIME,
+    prd_end_dt 			DATETIME
 );
 
 
-DROP TABLE IF EXISTS dw_bronze.crm_prd_info;
-CREATE TABLE dw_bronze.crm_prd_info (
-    prd_id 					INT,
-    prd_key 				VARCHAR(50),
-    prd_nm 					VARCHAR(80),
-    prd_cost 				INT,
-    prd_line 				VARCHAR(50),
-    prd_start_dt 			DATETIME,
-    prd_end_dt 				DATETIME
+DROP TABLE IF EXISTS db_bronze.crm_sales_details;
+CREATE TABLE db_bronze.crm_sales_details(
+	sls_order_nm 		NVARCHAR(30) ,
+    sls_prd_key  		NVARCHAR(30) ,
+    sls_cust_id 		INT,
+    sls_order_dt 		DATETIME,
+    sls_ship_dt			DATETIME,
+    sls_due_dt 			DATETIME,
+    sls_sales 			INT,
+    sls_quantity 		INT ,
+    sls_price 			INT
+);
+
+DROP TABLE IF EXISTS db_bronze.erp_cust_az12;
+CREATE TABLE db_bronze.erp_cust_az12(
+	cid 				NVARCHAR(30),
+    bdate 				DATETIME ,
+    gen  				NVARCHAR(20)
 );
 
 
-DROP TABLE IF EXISTS dw_bronze.crm_sales_details;
-CREATE TABLE dw_bronze.crm_sales_details (
-    sls_ord_num 			VARCHAR(50),
-    sls_prd_key 			VARCHAR(50),
-    sls_cust_id 			INT,
-    sls_order_dt 			INT,
-    sls_ship_dt 			INT,
-    sls_due_dt 				INT,
-    sls_sales 				INT,
-    sls_quantity 			INT,
-    sls_price 				INT
+
+DROP TABLE IF EXISTS db_bronze.erp_loc_a101;
+CREATE TABLE db_bronze.erp_loc_a101(
+	cid 				NVARCHAR(30),
+    cntry 				NVARCHAR(30)
 );
 
 
-DROP TABLE IF EXISTS dw_bronze.erp_cust_az12;
-CREATE TABLE dw_bronze.erp_cust_az12 (
-    cid 					VARCHAR(50),
-    bdate 					DATE,
-    gen 					VARCHAR(50)
+
+
+DROP TABLE IF EXISTS db_bronze.erp_px_cat_g1v2;
+CREATE TABLE db_bronze.erp_px_cat_g1v2(
+	id 					NVARCHAR(20),
+    cat 				NVARCHAR(30),
+    subcat 				NVARCHAR(30),
+    maintenance 		NVARCHAR(10)
 );
 
 
-DROP TABLE IF EXISTS dw_bronze.erp_loc_a101;
-CREATE TABLE dw_bronze.erp_loc_a101 (
-    cid 					VARCHAR(50),
-    cntry 					VARCHAR(50)
-);
 
 
-DROP TABLE IF EXISTS dw_bronze.erp_px_cat_g1v2;
-CREATE TABLE dw_bronze.erp_px_cat_g1v2 (
-    id 						VARCHAR(50),
-    cat 					VARCHAR(50),
-    subcat 					VARCHAR(50),
-    maintenance 			VARCHAR(50)
-);
+
+
+
+
